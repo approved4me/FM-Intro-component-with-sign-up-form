e3b70951f8c8efc42c4c4df020fcc5ce4f77ddae
approved4me <approved4me@gmail.com>
2023-01-08 08:24:47 +0100

    added return false to the if statements

--- script.js
+++ script.js
@@ -6,31 +6,34 @@ const form = document.getElementById("form");
 const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 form.addEventListener("submit", (e) => {
-    
+    e.preventDefault();
     if (email.value === "" || email.value == null || (!email.value.match(pattern))) {
-        e.preventDefault();
         email.classList.add("error");
         email.nextElementSibling.classList.remove("hide");
+        return false;
     }
 
     if (firstName.value === "") {
-        e.preventDefault();
         firstName.classList.add("error");
         firstName.nextElementSibling.classList.remove("hide");
+        return false;
     }
 
     if (lastName.value === "") {
-        e.preventDefault();
         lastName.classList.add("error");
         lastName.nextElementSibling.classList.remove("hide");
     }
 
     if (password.value === "") {
-        e.preventDefault();
         password.classList.add("error");
         password.nextElementSibling.classList.remove("hide");
+        return false;
     }
 
-    
+    else {
+        form.innerText = ("Thanks for your subscription!");
+        form.style.fontSize = "2.5rem";
+        form.style.color = "var(--primary-red)";
+    }
 
 });
